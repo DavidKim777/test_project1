b2ecc02810a3eff1ca06062ba309c42167b69377
DavidKim777 <dk4835125@gmail.com>
2023-12-20 12:45:05 +0200

    MCT-350: add cowboy

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 PROJECT = test_project
 PROJECT_DESCRIPTION = New project
 PROJECT_VERSION = 0.1.0
-DEPS = jsx
+DEPS = jsx cowboy
+dep_cowboy = git https://github.com/ninenines/cowboy  2.10.0
 dep_jsx = git  https://github.com/talentdeficit/jsx v3.1.0
 include erlang.mk
--- src/test_project_app.erl
+++ src/test_project_app.erl
@@ -5,7 +5,14 @@
 -export([stop/1]).
 
 start(_Type, _Args) ->
+	Dispatch = cowboy_router:compile([
+	 {'_',[{"/[...]", test_project_handler, []}]}
+]),
+	{ok, _} = cowboy:start_clear(test_project,
+		[{port, 8080}],
+		#{env => #{dispatch => Dispatch}}),
 	test_project_sup:start_link().
+
 
 stop(_State) ->
 	ok.
